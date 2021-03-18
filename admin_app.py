f91616ae77cfb8e281d5049c7a218f379a161f46
JAY Mathilde <mathilde.jay@imt-atlantique.net>
2021-03-18 18:47:19 +0100

    fix issue docker

--- admin_app.py
+++ admin_app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from TheGame.admin_utils import preprocess
+from admin_utils import preprocess
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.config.suppress_callback_exceptions = True
@@ -24,9 +24,9 @@ app.layout = html.Div([
                        style={"display":"none"},
                        id="name_{}".format(i)) for i in range(1,7)]),
     dbc.Button("Lancer la partie", id="validation_button", color="info", className="mr-1", disabled=True, style={"display":"none"}),
-    html.P(id="target")
+    html.P(id="target"),
+    dcc.Location(id='url', refresh=False)
 ], style={'margin':'10px'})
-
 
 
 @app.callback([Output("input_user","children"), Output("validation_button","style")] + \
@@ -41,14 +41,15 @@ def index_callback(val):
         return ["", hidden] + [hidden for i in range(1,7)]
 
 @app.callback(Output("target", "children"),
-              [Input("validation_button", "n_clicks")],
+              [Input("validation_button", "n_clicks"), Input('url', 'href')],
               [State('input_number_player', 'value')] + [State("name_{}".format(i), 'value') for i in range(1,7)])
-def index_callback(n_click, n_player, *vals):
+def index_callback(n_click, href_input,  n_player, *vals):
+
     if n_click is not None:
         preprocess(n_player, vals, dir='../data')
         L=[dcc.Markdown('''Envoi ces liens à tes coéquipiers:''')]
         for i in range(n_player):
-            href = "http://127.0.0.1:9001/player{}".format(i)
+            href = "{}9001/player{}".format(href_input.split('9000')[0], i)
             L = L + [dcc.Markdown('''{} : {}'''.format(vals[i], href))]
         return L
     else:
@@ -62,6 +63,6 @@ def index_callback(number_player, *vals):
             return False
     return True
 
-#@app.callback(,Input("validation_button", ))
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    #app.run_server(debug=True, port=9000)
+    app.run_server(debug=True, port=9000, host='0.0.0.0')
