3aa07037360c9f062d0cf08508545fef0869395b
vincentAGNES <mathilde.jay@imt-atlantique.net>
2021-03-25 19:19:33 +0100

    Add tests

--- appp.py
+++ appp.py
@@ -5,9 +5,10 @@ app = dash.Dash()
 app.config.suppress_callback_exceptions = True
 server = app.server
 
+
 app.layout = html.Div([
     html.Div("rrrr", id='page-content'),
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050, host='0.0.0.0')
+    app.run_server(debug=True, port=5000, host='0.0.0.0')
